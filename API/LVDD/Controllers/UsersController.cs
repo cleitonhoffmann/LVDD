5b0b29364219ffb30d512ef3a14ddaf8b42f8b85
cleiton.hoffmann <cleiton.hoffmann@digitaldesk.com.br>
2016-06-09 14:18:33 -0300

    API

---
+++ API/LVDD/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace LVDD.Controllers
+{
+    public class UsersController : ApiController
+    {
+        // GET api/users
+        public IEnumerable<Models.UserItem> Get()
+        {
+            var path = HttpContext.Current.Server.MapPath("~/App_Data/users.json");
+            var json = System.IO.File.ReadAllText(path);
+            var deserialized = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<Models.UserItem>>(json);
+
+            return deserialized;
+        }
+    }
+}
